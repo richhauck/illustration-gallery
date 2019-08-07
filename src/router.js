a453c14bd81bf2c34a3c31119bf5fc9c456eb157
Rich Hauck <rich@hauckinteractive.com>
2019-08-07 08:13:35 -0400

    Scaffolded routes and 404
    Signed-off-by: Rich Hauck <rich@hauckinteractive.com>

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import NotFound from './views/NotFound.vue';
 
 Vue.use(Router);
 
@@ -21,5 +22,17 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/art',
+      name: 'art',
+      // Lazy-load component
+      component: () => import('./views/Art.vue'),
+    },
+    {
+      path: '/404',
+      component: NotFound,
+      name: 'not-found'
+    },
+    { path: '*', redirect:'/404'}
   ],
 });
---
+++ src/views/Art.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Art Page</h1>
+  </div>
+</template>
